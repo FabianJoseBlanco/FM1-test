7ba962468560fa1aef55c382caef31c124f46def
Fabian Blanco <fabians_blanco@hotmail.com>
2022-08-03 21:12:52 -0500

    test n1

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,34 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-var fabian ="fabian";
+  let suma = 0;
+  for (let i = 0; i < num.length; i++) {
+     //suma = suma + +num[i] * 2 ** ((num.length-1 )- i);
+     suma = suma + num[i] * 2 ** ((num.length-1 )- i);
+  }
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  
+  var division=0; 
+  var binario=[]; 
+  
+  do { 
+      division=num/2; 
+      if (num%2==0) { 
+          binario.unshift("0"); 
+      } 
+      else { 
+          binario.unshift("1"); 
+      } 
+      num=Math.floor(division); 
+      }
+      while(num >= 1); 
+       binario=binario.join(""); 
 
+      return binario;
 }
 
 
